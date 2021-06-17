8415d264fbbe0bd1e8ca130d9fcc7ffdc630e529
Yauheni <kirichenkozhenia@mail.ru>
2021-06-17 13:18:04 +0300

    update mocks

--- project/src/index.js
+++ project/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
-import { getHotelsData } from './mock/mock.js';
+import { getHotelsData, getCommentsData } from './mock/mock.js';
 const HOTELS_COUNT = 5;
+const COMMENTS_COUNT = 5;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App hotels={getHotelsData(HOTELS_COUNT)} />
+    <App hotels={getHotelsData(HOTELS_COUNT)} comments={getCommentsData(COMMENTS_COUNT)} />
   </React.StrictMode>,
   document.getElementById('root'));
--- project/src/mock/mock.js
+++ project/src/mock/mock.js
@@ -1,13 +1,15 @@
 const cityNames = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'];
 const images = ['img/room.jpg', 'img/apartment-01.jpg', 'img/apartment-02.jpg', 'img/apartment-03.jpg', 'img/studio-01.jpg'];
-const avatars = ['img/avatar-angelina.jpg', 'img/avatar-max.jpg'];
+const avatars = ['img/avatar-angelina.jpg', 'img/avatar-max.jpg', 'img/avatar.svg'];
 const DESCRIPTION = 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.';
 const goods = ['Heating', 'Kitchen', 'Cable TV', 'Washing machine', 'Coffee machine', 'Dishwasher'];
+
 function generateId() {
   return 100 * Math.random();
 }
 
 let hotelId = 0;
+let commentId = 0;
 
 function getRandomIntegerRange(min = 0, max = 10) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -47,4 +49,34 @@ export function getHotelData() {
 
 export function getHotelsData(count) {
   return Array(count).fill().map(() => getHotelData());
+}
+
+export function getCommentData() {
+  return {
+    comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    date: '2019-05-08T14:13:56.569Z',
+    id: commentId++,
+    rating: 4,
+    user: {
+      avatarUrl: avatars[1],
+      id: generateId(),
+      isPro: Boolean(Math.round(Math.random())),
+      name: 'Max',
+    },
+  };
+}
+
+export function getCommentsData(count) {
+  return Array(count).fill().map(() => getCommentData());
+}
+
+export function getUserData() {
+  return {
+    avatarUrl: avatars[2],
+    email: 'Oliver.conner@gmail.com',
+    id: 1,
+    isPro: false,
+    name: 'Oliver.conner',
+    token: 'T2xpdmVyLmNvbm5lckBnbWFpbC5jb20=',
+  };
 }
